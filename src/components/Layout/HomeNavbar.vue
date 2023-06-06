027ebf04525fc2094839f9bc665d7a85876ac1e2
anangyoga <aharwinto@gmail.com>
2023-06-06 22:27:10 +0700

    feat: sign-in & add product component

--- src/components/Layout/HomeNavbar.vue
+++ src/components/Layout/HomeNavbar.vue
@@ -3,9 +3,9 @@
 <template>
   <nav class="container flex flex-col pt-4 lg:flex-row lg:items-center lg:justify-between lg:pt-10">
     <div class="flex items-center justify-between mb-6 lg:mb-0">
-      <a href="/">
+      <RouterLink to="/">
         <img src="@/assets/svg/app-logo.svg" class="w-full" alt="" />
-      </a>
+      </RouterLink>
 
       <a href="javascript:void(0)" class="w-6 h-6 mobile-tablet" id="navbarToggler">
         <img src="@/assets/svg/ic-hamburger.svg" alt="" />
@@ -17,7 +17,7 @@
         class="flex flex-col justify-between w-full gap-4 lg:mx-auto lg:flex-row lg:items-center"
       >
         <div class="flex flex-col lg:mx-auto lg:items-center lg:flex-row gap-x-10 gap-y-4">
-          <a href="#" class="nav-link active"> Home </a>
+          <RouterLink to="/" class="nav-link active"> Home </RouterLink>
           <a href="#" class="nav-link"> Features </a>
           <a href="#" class="nav-link"> Pricing </a>
           <a href="#" class="nav-link"> Case study </a>
