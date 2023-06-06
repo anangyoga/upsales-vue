718550d6fb120181a2e4329c265d7ff1ae589438
anangyoga <aharwinto@gmail.com>
2023-06-06 22:28:20 +0700

    vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignUpView from '@/views/Auth/SignUpView.vue'
+import AddProductView from '@/views/Auth/AddProductView.vue'
+import DashboardViewVue from '@/views/Dashboard/DashboardView.vue'
+import ProductView from '@/views/Dashboard/Product/ProductView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,15 +12,27 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView
+    },
+    {
+      path: '/add-product',
+      name: 'add-product',
+      component: AddProductView
+    },
+    {
+      path: '/dashboard',
+      name: 'dashboard',
+      component: DashboardViewVue
+    },
+    {
+      path: '/products',
+      name: 'products',
+      component: ProductView
     }
-    // {
-    //   path: '/about',
-    //   name: 'about',
-    //   // route level code-splitting
-    //   // this generates a separate chunk (About.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () => import('../views/AboutView.vue')
-    // }
   ]
 })
 
