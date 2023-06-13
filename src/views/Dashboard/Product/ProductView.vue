5a9ea7f38d7b47599c844747b1993b4fc11afd4f
anangyoga <aharwinto@gmail.com>
2023-06-13 15:44:12 +0700

    feat: product add

--- src/views/Dashboard/Product/ProductView.vue
+++ src/views/Dashboard/Product/ProductView.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
 import ProductHeader from '@/components/Dashboard/Product/ProductHeader.vue'
 import ProductTable from '@/components/Dashboard/Product/ProductTable.vue'
-import DashboardSidebar from '@/components/Layout/DashboardSidebar.vue'
-import DashboardTopBar from '@/components/Layout/DashboardTopBar.vue'
 </script>
 
 <template>
-  <div class="flex min-h-screen pb-10">
-    <DashboardSidebar :activeMenu="'products'" />
-    <main class="lg:pt-[50px] lg:px-[50px] p-4 w-full lg:border-l lg:border-l-borderLight">
-      <DashboardTopBar />
-      <ProductHeader />
-
-      <section class="w-full min-h-[90vh] p-4 bg-subtleGrey rounded-2xl flex flex-col">
-        <ProductTable />
-      </section>
-    </main>
-  </div>
+  <ProductHeader />
+  <section class="w-full min-h-[90vh] p-4 bg-subtleGrey rounded-2xl flex flex-col">
+    <ProductTable />
+  </section>
 </template>
